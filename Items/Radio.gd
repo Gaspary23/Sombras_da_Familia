bdd133d47b612739829274f110c3804c875e5ae5
Pedro Gaspary <gaspary2310@gmail.com>
2023-05-22 18:07:37 -0300

    fix: was and radio

--- Items/Radio.gd
+++ Items/Radio.gd
@@ -2,17 +2,14 @@ extends Area2D
 
 onready var sound = $Monster_Theme
 var touchingPlayer = false
-var playing = false
 
 
 func _process(_delta: float) -> void:
-	if (Input.is_action_just_pressed("interact") and touchingPlayer):
-		
-		playing = not playing
-		print("R:",playing)
-		
-	if (playing):
-		sound.play()
+	if (Input.is_action_just_pressed("interact") and touchingPlayer):		
+		if (not sound.is_playing()):
+			sound.play()
+		else:
+			sound.stop()
 
 
 func _on_Radio_body_entered(_body: Node) -> void:
--- Items/Washing_Machine.gd
+++ Items/Washing_Machine.gd
@@ -2,17 +2,14 @@ extends Area2D
 
 onready var sound = $Washing
 var touchingPlayer = false
-var playing = false
 
 
 func _process(_delta: float) -> void:
 	if (Input.is_action_just_pressed("interact") and touchingPlayer):
-		
-		playing = not playing
-		print("W:",playing)
-		
-	if (playing):
-		sound.play()
+		if (not sound.is_playing()):
+			sound.play()
+		else:
+			sound.stop()
 
 
 func _on_Washing_Machine_body_entered(_body: Node) -> void:
