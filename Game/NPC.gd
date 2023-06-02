e9e5fe3170e4febc28681a287f81062a1898c0c1
LucasMCunha <lucascunha02@hotmail.com>
2023-06-02 18:54:32 -0300

    NPC AI strated

---
+++ Game/NPC.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+enum State {action,waitng,checking,walking}
+
+var levelOfSuspission: int = 0
+var taskTime: int = 0
+var currentState: int = State.waiting
+	
+
+func scripted() -> int:
+	if(currentState	== State.waitng):
+		yield(get_tree().create_timer(1), "timeout")
+		return 0
+	if(currentState	== State.action):
+		return 0
+	if(currentState	== State.checking):
+		return 0
+	if(currentState	== State.walking):
+		return 0
+	return 0
+	
+	
+func _ready():
+	pass 
---
+++ StateMachine.gd
@@ -0,0 +1,5 @@
+extends Node
+
+class_name States
+
+enum State {action,waitng,checking}
