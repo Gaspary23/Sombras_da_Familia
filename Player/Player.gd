788a59426b81064bd9738e3e21aedfaa2d66aec5
Pedro Gaspary <gaspary2310@gmail.com>
2023-06-12 19:02:18 -0300

    bugged: hide tween

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,8 @@ onready var target = position
 onready var sprite = $Sprite
 onready var door_sound = $Door
 var motion = Vector2.ZERO
+var hidden = false
+var tween
 
 
 func get_side_input():
@@ -23,7 +25,7 @@ func get_side_input():
 
 
 func squash_and_stretch():
-	var tween = create_tween()
+	tween = create_tween()
 	tween.tween_property(sprite, "scale", Vector2(1.25,0.75), 0.1)
 	tween.tween_property(sprite, "scale", Vector2(1,1), 0.1)
 
@@ -32,9 +34,15 @@ func _physics_process(_delta):
 	if (Input.is_action_just_pressed("interact")):
 		squash_and_stretch()
 		if(is_on_wall()):
-			visible = not visible
 			door_sound.play()
-		
-	if(visible):
+			print(hidden)
+			hidden = not hidden
+			print(hidden, "\n")
+			tween.tween_property(sprite, "visible", hidden, 0.1)
+	
+	if(not hidden):
+		visible = true
 		get_side_input()
 		motion = move_and_slide(motion, Vector2.UP)
+	else:
+		visible = false
