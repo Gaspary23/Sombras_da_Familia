68ba5dde8753f395f49dd96d9a5686489e6aae0f
Pedro Gaspary <gaspary2310@gmail.com>
2023-06-12 18:30:18 -0300

    feat: interact feedback

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,17 +6,15 @@ export (int) var speed := 200
 onready var target = position
 onready var sprite = $Sprite
 onready var door_sound = $Door
-
-var velocity = Vector2.ZERO
-var rotation_dir = 0
+var motion = Vector2.ZERO
 
 
 func get_side_input():
-	velocity.x = Input.get_action_strength("right")-Input.get_action_strength("left")
-	velocity.x *= speed
-	if velocity.x > 0:
+	motion.x = Input.get_action_strength("right")-Input.get_action_strength("left")
+	motion.x *= speed
+	if motion.x > 0:
 		sprite.play("right")
-	elif velocity.x < 0:
+	elif motion.x < 0:
 		sprite.play("left")
 	else:
 		sprite.stop()
@@ -24,13 +22,19 @@ func get_side_input():
 		sprite.frame = 0
 
 
+func squash_and_stretch():
+	var tween = create_tween()
+	tween.tween_property(sprite, "scale", Vector2(1.25,0.75), 0.1)
+	tween.tween_property(sprite, "scale", Vector2(1,1), 0.1)
+
+
 func _physics_process(_delta):
-	if (is_on_wall() and Input.is_action_just_pressed("interact")):
-		visible = not visible
-		door_sound.play()
+	if (Input.is_action_just_pressed("interact")):
+		squash_and_stretch()
+		if(is_on_wall()):
+			visible = not visible
+			door_sound.play()
 		
 	if(visible):
 		get_side_input()
-		velocity = move_and_slide(velocity, Vector2.UP)
-		
-	
+		motion = move_and_slide(motion, Vector2.UP)
