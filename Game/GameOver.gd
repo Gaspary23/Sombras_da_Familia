7843f01e985e0c8d16b157f1e0754c1ab9ff9d84
Pedro Gaspary <gaspary2310@gmail.com>
2023-06-29 20:05:07 -0300

    feat: pause menu

--- Game/GameOver.gd
+++ Game/GameOver.gd
@@ -2,7 +2,22 @@ extends Control
 
 onready var sound = $ButtonClick
 
-func _on_Button_pressed():
+
+func _ready():
+	$VBoxContainer/Try_Again.grab_focus()
+
+
+func _on_Try_Again_pressed():
 	sound.play()
 	yield(get_tree().create_timer(0.1), "timeout")
 	get_tree().change_scene("res://Game/Game.tscn")
+
+
+func _on_Return_to_Menu_pressed():
+	sound.play()
+	yield(get_tree().create_timer(0.1), "timeout")
+	get_tree().change_scene("res://Game/Menu.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
