0daead04b46ab402fcfc7bd3665a5034a647f723
Pedro Gaspary <gaspary2310@gmail.com>
2023-06-29 18:21:16 -0300

    feat: main menu

---
+++ Game/Menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Game/Game.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
